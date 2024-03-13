befd2152957fdea2281a1c0b6a16107729db1081
hafez adel <103792766+hafez599@users.noreply.github.com>
2024-03-13 07:15:11 +0200

    اللهم صلي علي محمد

--- Assets/Scripts/samuraiBoss/Boss.cs
+++ Assets/Scripts/samuraiBoss/Boss.cs
@@ -7,6 +7,8 @@ namespace SamuraiBoss
 {
     public class Boss : MonoBehaviour
     {
+        public Transform player;
+	    public bool isFlipped = false;
         public int maxHealth = 100;
         public int currentHealth;
         public SamuraiBoss.health_bar healthBar;
@@ -18,17 +20,40 @@ namespace SamuraiBoss
         }
 
         void Update()
-        {
+        {   
+            //GetComponent<Animator>().SetBool("hurt",false);
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 TakeDamage(20);
+                GetComponent<Animator>().SetBool("hurt",true);
             }
+            
         }
 
         void TakeDamage(int damage)
         {
             currentHealth -= damage;
             healthBar.SetHealth(currentHealth);
+        }
+
+
+        public void LookAtPlayer()
+        {
+            Vector3 flipped = transform.localScale;
+            flipped.z *= -1f;
+
+            if (transform.position.x > player.position.x && isFlipped)
+            {
+                transform.localScale = flipped;
+                transform.Rotate(0f, 180f, 0f);
+                isFlipped = false;
+            }
+            else if (transform.position.x < player.position.x && !isFlipped)
+            {
+                transform.localScale = flipped;
+                transform.Rotate(0f, 180f, 0f);
+                isFlipped = true;
+            }
         }
     }
 }
--- Assets/Scripts/samuraiBoss/Boss_run.cs
+++ Assets/Scripts/samuraiBoss/Boss_run.cs
@@ -1,23 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+namespace SamuraiBoss
+{
 public class Boss_run : StateMachineBehaviour
 {
     Transform player;
     public float speed = 2f;
     Rigidbody2D rb;
-    public float attackRange = 3f;
+    public float attackRange = 5f;
+    public SamuraiBoss.Boss boss;
     //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
        player = GameObject.FindGameObjectWithTag("pip").transform;
        rb = animator.GetComponent<Rigidbody2D>();
+       boss = animator.GetComponent<Boss>();
     }
 
     //OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {   
+        boss.LookAtPlayer();
         Vector2 target = new Vector2(player.position.x,rb.position.y);
         Vector2 newpos = Vector2.MoveTowards(rb.position,target,speed *Time.fixedDeltaTime );
         rb.MovePosition(newpos);
@@ -33,6 +37,5 @@ public class Boss_run : StateMachineBehaviour
     {
        animator.ResetTrigger("attack");
     }
-    
-    
+}
 }
