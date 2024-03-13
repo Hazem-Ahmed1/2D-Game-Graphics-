832eacd99abc114ddb91f8d0a86ce00b153cb5b9
Bedo-Mostafa <bedomostafa37@gmail.com>
2024-03-13 18:09:26 +0200

    ADD NPC

---
+++ Assets/Scripts/NPC/Player_move.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if (Input.GetAxis("Horizontal") > 0)
+        // {
+            if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+            {
+                // transform.position =new Vector3(transform.position.x + 2 , transform.position.y, transform.position.z);
+                transform.Translate(new Vector3(-0.07f,0,0));
+            }
+            else if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+            {
+                // mySprite.flipX = false;
+                transform.Translate(new Vector3(0.07f,0,0));
+            } 
+            // if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+            // {
+            //     // transform.position =new Vector3(transform.position.x + 2 , transform.position.y, transform.position.z);
+            //     transform.Translate(new Vector3(0,0.07f,0));
+            // }
+            // else if(Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+            // {
+            //     // mySprite.flipX = false;
+            //     transform.Translate(new Vector3(0,-0.07f,0));
+            // } 
+        // }
+    }
+}
