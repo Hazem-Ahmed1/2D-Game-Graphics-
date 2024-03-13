4d70b4ba3eb950bd27010ab793a6d9eb2e4babb5
toqafotoh <100733611+toqafotoh@users.noreply.github.com>
2024-03-14 00:44:29 +0200

    aywa ba2aa

--- Assets/Scripts/mainCharacter/Character_main.cs
+++ Assets/Scripts/mainCharacter/Character_main.cs
@@ -6,16 +6,24 @@ public class Character : MonoBehaviour
     // But "SerializeFiel" let you acess your private vars from the gui
     private Rigidbody2D body;
     private Animator anim;
+    private BoxCollider2D box;
+    [SerializeField] private LayerMask groundLayer;
+
     private bool isFacingright = true;
     private bool isRunning;
-    private BoxCollider2D box;
-    float horizontalInput;
-    //[SerializeField] private LayerMask ground;
-    [SerializeField] private LayerMask groundLayer;
+    private float horizontalInput;
+
+    [Header("Movement")]
     [SerializeField] private float speed;
     [SerializeField] private float jumpForce;
 
-
+    [Header("WallSliding")]
+    [SerializeField] private float wallSlidingSpeed;
+    [SerializeField] private LayerMask wallLayer;
+    [SerializeField] public Transform wallCheck;
+    [SerializeField] Vector2 wallCheckSize;
+    [SerializeField] private bool isTouchingWall;
+    [SerializeField] private bool isWallSliding;
 
     // Start method is called once before the first frame update
     // Awake method executed once when the game starts even if the component wasen't called
@@ -31,10 +39,12 @@ public class Character : MonoBehaviour
     void Update()
     {
         CheckInput();
+        WallSlide();
         Move();
         CheckDirection();
         UpdateAnimation();
     }
+
     private void CheckInput()
     {
         // Access the horizontal component from the input manager
@@ -47,7 +57,7 @@ public class Character : MonoBehaviour
     private void Move()
     {
         // Change the running speed and directions
-        // Get axis horizontal allow the user to move left and right by the keyboard (-1,1)
+        if(!isWallSliding) // Get axis horizontal allow the user to move left and right by the keyboard (-1,1)
         body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
         if (body.velocity.x != 0)
         {
@@ -81,12 +91,35 @@ public class Character : MonoBehaviour
     }
     private bool isGrounded()
     {
-        return Physics2D.BoxCast(box.bounds.center, box.bounds.size ,0f, Vector2.down, 0.1f, groundLayer);
+        return Physics2D.BoxCast(box.bounds.center, box.bounds.size, 0f, Vector2.down, 0.1f, groundLayer);
+    }
+    private bool isWalled()
+    {
+       return isTouchingWall = Physics2D.OverlapBox(wallCheck.position, wallCheckSize,0 ,wallLayer);
+    }
+    private void WallSlide()
+    {
+        if (isWalled() && !isGrounded() && body.velocity.y < 0)
+        {
+            isWallSliding = true;
+            //Debug.Log(isTouchingWall);
+            body.velocity = new Vector2(body.velocity.x,wallSlidingSpeed);
+        }
+        else
+        {
+            isWallSliding = false;
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube(wallCheck.position, wallCheckSize);
     }
     private void UpdateAnimation()
     {
         anim.SetBool("isRunning",isRunning);
         anim.SetBool("isGrounded", isGrounded());
         anim.SetFloat("yVelocity", body.velocity.y);
+        anim.SetBool("isSliding", isWallSliding);
     }
 }
