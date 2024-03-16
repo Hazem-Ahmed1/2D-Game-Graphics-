f1c611803a33cd34fa4454cdb03d0f44f74f1316
gimmeursocks <bobteen1@gmail.com>
2024-03-17 01:13:11 +0200

    works now

--- Assets/Scripts/NPCRun.cs
+++ Assets/Scripts/NPCRun.cs
@@ -1,30 +1,30 @@
 using UnityEngine;
-public class BossRun : StateMachineBehaviour
+public class NPCRun : StateMachineBehaviour
 {
     Transform player;
     Rigidbody2D _rb;
-    public Boss boss;
+    public NPC _npc;
     //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
        player = GameObject.FindGameObjectWithTag("Player").transform;
        _rb = animator.GetComponent<Rigidbody2D>();
-       boss = animator.GetComponent<Boss>();
+       _npc = animator.GetComponent<NPC>();
     }
 
     //OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {   
-        if (Vector2.Distance(player.position, _rb.position) <= boss._atkRange)
+        if (Vector2.Distance(player.position, _rb.position) <= _npc._atkRange)
         {
             animator.Play("attack");
         }
-        else if (Vector2.Distance(player.position, _rb.position) <= boss._lookRange)
+        else if (Vector2.Distance(player.position, _rb.position) <= _npc._lookRange)
 		{
 			animator.Play("walk");
-            boss.LookAtPlayer();
+            _npc.LookAtPlayer();
             Vector2 target = new(player.position.x,_rb.position.y);
-            Vector2 newpos = Vector2.MoveTowards(_rb.position, target, boss._speed * Time.fixedDeltaTime );
+            Vector2 newpos = Vector2.MoveTowards(_rb.position, target, _npc._speed * Time.fixedDeltaTime);
             _rb.MovePosition(newpos);  
 		}
         else animator.Play("idle");   
