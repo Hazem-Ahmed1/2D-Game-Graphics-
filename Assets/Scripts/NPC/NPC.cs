960af640afc3abe4095d8f6c9a1c0033932a0b09
gimmeursocks <bobteen1@gmail.com>
2024-03-29 21:20:23 +0200

    msh 3arf

--- Assets/Scripts/NPC/NPC.cs
+++ Assets/Scripts/NPC/NPC.cs
@@ -6,20 +6,17 @@ using UnityEngine;
         protected Transform player;
         protected Rigidbody2D rb;
         protected Animator animator;
-        private string currentAnimaton;
         public NPCAttributes attributes;
         private bool isFlipped = false;
         private bool isAttacking = false;
         public int currHealth;
-        const string NPC_IDLE = "idle";
-        const string NPC_RUN = "run";
-        const string NPC_ATTACK_1 = "atk_1";
+
         void Start()
         {
             rb = GetComponent<Rigidbody2D>();
             animator = GetComponent<Animator>();
             player = GameObject.FindGameObjectWithTag("Player").transform;
-            currHealth = attributes.healthPoints;
+            // currHealth = attributes.healthPoints;
         }
 
         void Update()
@@ -33,69 +30,56 @@ using UnityEngine;
             animator = GetComponent<Animator>();
             player = GameObject.FindGameObjectWithTag("Player").transform;
             
+            LookAtPlayer();
             BasicMovement();
         }
 
         protected void BasicMovement()
         {
-            LookAtPlayer();
-
-            rb = GetComponent<Rigidbody2D>();
-            animator = GetComponent<Animator>();
-            player = GameObject.FindGameObjectWithTag("Player").transform;
-
             float distanceToPlayer = Vector2.Distance(player.position, rb.position);
 
             if (distanceToPlayer < attributes.atkRange && !isAttacking)
             {
-                ChangeAnimationState(NPC_ATTACK_1);
                 isAttacking = true;
+                animator.SetBool("isAttacking", true);
                 Invoke("AttackDone", animator.GetCurrentAnimatorStateInfo(0).length);
             }
             else if (distanceToPlayer < attributes.lookRange && !isAttacking)
             {
-                ChangeAnimationState(NPC_RUN);
+                animator.SetBool("isRunning", true);
                 Vector2 target = new(player.position.x, rb.position.y);
                 Vector2 newpos = Vector2.MoveTowards(rb.position, target, attributes.speed * Time.fixedDeltaTime);
                 rb.MovePosition(newpos);
             }
-            else if (!isAttacking)
+            else
             {
-                ChangeAnimationState(NPC_IDLE);
+                animator.SetBool("isRunning", false);
             }
         }
 
     void TakeDamage(int damage)
+    {
+        currHealth -= damage;
+        if(currHealth <= 0)
         {
-            currHealth -= damage;
-            if(currHealth <= 0)
-            {
-                Destroy(gameObject);
-            }
-        }
-
-
-        public void LookAtPlayer()
-        {
-            if ((transform.position.x > player.position.x && isFlipped) || 
-                (transform.position.x < player.position.x && !isFlipped))
-            {
-                transform.Rotate(0f, 180f, 0f);
-                isFlipped = !isFlipped;
-            }
-        }
-
-        void ChangeAnimationState(string newAnimation)
-        {
-            if (currentAnimaton == newAnimation) return;
-
-            animator.Play(newAnimation);
-            Debug.Log(newAnimation+" is being played");
-            currentAnimaton = newAnimation;
-        }
-
-        void AttackDone()
-        {
-            isAttacking = false;
+            Destroy(gameObject);
         }
     }
+
+
+    public void LookAtPlayer()
+    {
+        if ((transform.position.x > player.position.x && isFlipped) || 
+            (transform.position.x < player.position.x && !isFlipped))
+        {
+            transform.Rotate(0f, 180f, 0f);
+            isFlipped = !isFlipped;
+        }
+    }
+
+    void AttackDone()
+    {
+        isAttacking = false;
+        animator.SetBool("isAttacking", false);
+    }
+}
