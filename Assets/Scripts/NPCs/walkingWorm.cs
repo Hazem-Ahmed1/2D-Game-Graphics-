71bd88eee36b567b52d88975ac1a8c8e1f61f3a0
Jomana Mohammed <jomanamohammed816@gmail.com>
2024-04-09 21:43:40 +0200

    edit worm

--- Assets/Scripts/NPCs/walkingWorm.cs
+++ Assets/Scripts/NPCs/walkingWorm.cs
@@ -5,122 +5,39 @@ using UnityEngine;
 public class walkingWorm : MonoBehaviour
 {
 
-    /*    public float moveSpeed = 5f; // Adjust the speed as needed
-        public float groundRange = 8f; // Range of movement on the ground
-        [SerializeField] private LayerMask groundLayer;
-        private Rigidbody2D body;
-        public Animator anim;
-        private BoxCollider2D boxCollider;
-        private SpriteRenderer spriteRenderer;
-        private float horizontalInput;
-        private bool isFacingRight = true; //default
-
-        private void Awake()
-        {
-            //Grab references for rigidbody and animator from object
-            body = GetComponent<Rigidbody2D>();
-            anim = GetComponent<Animator>();
-            boxCollider = GetComponent<BoxCollider2D>();
-            spriteRenderer = GetComponent<SpriteRenderer>();
-        }
-
-        void Update()
-        {
-            //default that the worm is to walk 
-            anim.SetBool("walkWorm", true);
-
-            // Move the character
-            MoveCharacter();
-
-            // Check if the character needs to flip
-            CheckFlip();
-        }
-
-        void MoveCharacter()
-        {
-            // Move the character horizontally
-            float horizontalInput = isFacingRight ? 1f : -1f;
-            body.velocity = new Vector2(horizontalInput * moveSpeed, body.velocity.y);
-        }
-
-        void CheckFlip()
-        {
-            // Get the position of the character
-            Vector2 currentPosition = transform.position;
-
-            // Check if the character is reaching the edge of the ground range
-            if (currentPosition.x >= groundRange && isFacingRight || currentPosition.x <= -groundRange && !isFacingRight)
-            {
-                // Flip the character
-                FlipCharacter();
-            }
-        }
-
-        void FlipCharacter()
-        {
-            // Flip the character horizontally
-            isFacingRight = !isFacingRight; 
-            spriteRenderer.flipX = !spriteRenderer.flipX;
-        }*/
-
-
-    PlayerMovement playerclass;
-    public float moveSpeed = 5f; // Adjust the speed as needed for player
-    public float speed = 5f;
-    public float groundRange = 5f; // Range for NPC of movement on the ground
-    [SerializeField] private LayerMask groundLayer;
+    public float moveSpeed = 5f; // Adjust the speed as needed
     private Rigidbody2D body;
     public Animator anim;
     private BoxCollider2D boxCollider;
     private SpriteRenderer spriteRenderer;
     private float horizontalInput;
     private bool isFacingRight = true; //default
-
-    private Transform player;
-    public float lineOfSite = 16;
-
-    Collision2D collision;
+    public float lineOfSite = 3;
+    public Transform startPoint;
+    public Transform endPoint;
 
 
     private void Awake()
     {
-        player = GameObject.FindGameObjectWithTag("player").transform;
         //Grab references for rigidbody and animator from object
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         boxCollider = GetComponent<BoxCollider2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        startPoint = GameObject.FindGameObjectWithTag("startPoint").transform;
+        endPoint = GameObject.FindGameObjectWithTag("endPoint").transform;
     }
 
     void Update()
     {
-        body.freezeRotation = true;
+        //default that the worm is to walk 
+        anim.SetBool("walkWorm", true);
+
+        // Move the character
         MoveCharacter();
-        // Get the position of the character
-        Vector2 currentPosition = transform.position;
 
-        float distanceFromThePlayer = Vector2.Distance(player.position, this.transform.position);
-
-        // Check if the player entered the character range 
-        if (distanceFromThePlayer <= lineOfSite)
-        {
-            checkPlayerDirection();
-            //anim.SetBool("runTowardsPlayer", true);  //correct
-            moveSpeed = 8f;
-            OnCollisionStay2D(collision); //correct // if the player and character are collided the character will attack the player 
-
-        }
-        else if (distanceFromThePlayer > lineOfSite) // Check if the character is reaching the edge of the ground range to flip
-        {
-            //anim.SetBool("runTowardsPlayer", false);
-            moveSpeed = 5f;
-            if (currentPosition.x >= groundRange && isFacingRight || currentPosition.x <= -groundRange && !isFacingRight)
-            {
-                // Flip the character
-                FlipCharacter();
-            }
-        }
-
+        // Check if the character needs to flip
+        CheckFlip();
     }
 
     void MoveCharacter()
@@ -128,8 +45,33 @@ public class walkingWorm : MonoBehaviour
         // Move the character horizontally
         float horizontalInput = isFacingRight ? 1f : -1f;
         body.velocity = new Vector2(horizontalInput * moveSpeed, body.velocity.y);
+    }
+
+    void CheckFlip()
+    {
+
+        float distanceFromStartPoint = Vector2.Distance(startPoint.position, this.transform.position);
+        float distanceFromEndPoint = Vector2.Distance(endPoint.position, this.transform.position);
+
+
+        if (distanceFromStartPoint <= lineOfSite && isFacingRight == false)
+        {
+            FlipCharacter();
+        }
+        else if (distanceFromEndPoint <= lineOfSite && isFacingRight == true)
+        {
+            FlipCharacter();
+        }
+    }
+
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, lineOfSite);
 
     }
+
 
     void FlipCharacter()
     {
@@ -141,34 +83,6 @@ public class walkingWorm : MonoBehaviour
         this.transform.localScale = myScale;
     }
 
-    void checkPlayerDirection() //if scale.X is +ve the character is facing right , is -ve facing left.
-    {
-        if (player.transform.position.x < gameObject.transform.position.x && isFacingRight)
-            FlipCharacter();
-        if (player.transform.position.x > gameObject.transform.position.x && !isFacingRight)
-            FlipCharacter();
-    }
-    private void OnDrawGizmosSelected()
-    {
-        Gizmos.color = Color.green;
-        Gizmos.DrawWireSphere(transform.position, lineOfSite);
 
-    }
-
-    private void OnDrawGizmos() //not necessary
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, groundRange);
-    }
-
-    private void OnCollisionStay2D(Collision2D collision)
-    {
-        if (collision.gameObject.name == "player")
-            anim.SetBool("attackPlayer", true);
-        else
-        {
-            anim.SetBool("attackPlayer", false); // IMP. condition
-        }
-
-    }
 }
+
